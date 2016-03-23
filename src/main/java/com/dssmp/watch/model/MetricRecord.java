ded563b7c6636b15d7842f01cd04509a9e01181b
x010 <jeffrey@JeffreydeMacBook-Pro.local>
2016-03-23 15:50:53 +0800

    metic model

---
+++ src/main/java/com/dssmp/watch/model/MetricRecord.java
@@ -0,0 +1,126 @@
+package com.dssmp.watch.model;
+
+import java.util.Date;
+
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+public class MetricRecord {
+    /**
+     * ID
+     */
+    private long id;
+
+    /**
+     * 命名空间ID
+     */
+    private long nid;
+
+    /**
+     * 命名空间
+     */
+    private String namespace;
+
+    /**
+     * MetricRecord ID
+     */
+    private long mid;
+
+    /**
+     * 指标ID
+     */
+    private String meticname;
+
+    /**
+     * 开始
+     */
+    private Date starttime;
+
+    /**
+     * 结束时间
+     */
+    private Date endtime;
+
+    /**
+     * 值
+     */
+    private double value;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public long getNid() {
+        return nid;
+    }
+
+    public void setNid(long nid) {
+        this.nid = nid;
+    }
+
+    public String getNamespace() {
+        return namespace;
+    }
+
+    public void setNamespace(String namespace) {
+        this.namespace = namespace;
+    }
+
+    public long getMid() {
+        return mid;
+    }
+
+    public void setMid(long mid) {
+        this.mid = mid;
+    }
+
+    public String getMeticname() {
+        return meticname;
+    }
+
+    public void setMeticname(String meticname) {
+        this.meticname = meticname;
+    }
+
+    public Date getStarttime() {
+        return starttime;
+    }
+
+    public void setStarttime(Date starttime) {
+        this.starttime = starttime;
+    }
+
+    public Date getEndtime() {
+        return endtime;
+    }
+
+    public void setEndtime(Date endtime) {
+        this.endtime = endtime;
+    }
+
+    public double getValue() {
+        return value;
+    }
+
+    public void setValue(double value) {
+        this.value = value;
+    }
+}
